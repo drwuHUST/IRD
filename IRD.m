989ff60757d6f5c8c2bf01405cf028bf51fcfca9
Dongrui Wu <50743243+drwuHUST@users.noreply.github.com>
2020-10-12 18:02:14 -0400

    Add files via upload

--- IRD.m
+++ IRD.m
@@ -2,13 +2,19 @@ function idsTrain = IRD(X, d, Cmax)
 
 %% Implementation of the IRD algorithm in the following paper:
 %%
-%% 刘子昂, 蒋雪, 伍冬睿*, "基于池的无监督线性回归主动学习," 自动化学报, 2020.
+%% 刘子昂, 蒋雪, 伍冬睿, "基于池的无监督线性回归主动学习," 自动化学报, 2020.
 %% LIU Zi-Ang, JIANG Xue, WU Dong-Rui. Unsupervised Pool-Based Active Learning for Linear Regression.
 %% Acta Automatica Sinica, 2020.
 %%
 %% Or the English version here: https://arxiv.org/pdf/2001.05028.pdf
 %%
-%% Dongrui Wu, drwu@hust.edu.cn
+%% X: Input data matrix; each sample is a row vector
+%% d: number of samples to be selected by IRD
+%% Cmax: number of iterative updates; c_max in the paper
+%% IdsTrain: selected sample indices
+%%
+%% Dongrui WU, drwu@hust.edu.cn
+
 
 [N, M] = size(X);
 
